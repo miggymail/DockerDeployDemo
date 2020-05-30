e95107740c9f8b1ac1873b446083de802fa2da97
Miguel T <miggymail@gmail.com>
2020-05-30 16:59:56 -0700

    Initial Commit

---
+++ DockerUI/Utilities/ISqlLiteDataAccess.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace DockerUI.Utilities
+{
+    public interface ISqlLiteDataAccess
+    {
+        List<T> LoadData<T>(string sql);
+        void SaveData<T>(T person, string sql);
+        void UpdateData<T>(T person, string sql);
+    }
+}
---
+++ DockerUI/Utilities/SqlLiteDataAccess.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+using System.Data;
+using Dapper;
+using System.Configuration;
+
+namespace DockerUI.Utilities
+{
+    public class SqlLiteDataAccess : ISqlLiteDataAccess
+    {
+        //Test SqlLite
+        public List<T> LoadData<T>(string sql)
+        {
+            using (IDbConnection cnn = new SQLiteConnection(LoadConnectionString()))
+            {
+                var output = cnn.Query<T>(sql, new DynamicParameters());
+                return output.ToList();
+            }
+        }
+
+        public void SaveData<T>(T person, string sql)
+        {
+            using (IDbConnection cnn = new SQLiteConnection(LoadConnectionString()))
+            {
+                cnn.Execute(sql, person);
+            }
+        }
+
+        public void UpdateData<T>(T person, string sql)
+        {
+            using (IDbConnection cnn = new SQLiteConnection(LoadConnectionString()))
+            {
+                cnn.Execute(sql, person);
+            }
+        }
+
+
+        private string LoadConnectionString(string id = "Default")
+        {
+            return ConfigurationManager.ConnectionStrings[id].ConnectionString;
+        }
+    }
+}
